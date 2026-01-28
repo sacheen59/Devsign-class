01b3613ba1a8559b3253a0aa97f52d580b053982
Sachin Barali <baralisachin5925@gmail.com>
2026-01-28 13:20:37 +0545

    category list

--- batch2/ecommerce/product/urls.py
+++ batch2/ecommerce/product/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('',views.product_page)
+   path('',views.product_page),
+   path('categories/',views.get_all_cateogries),
 ]
--- batch2/ecommerce/product/views.py
+++ batch2/ecommerce/product/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product,Category
 
 # Create your views here.
 
@@ -7,4 +7,10 @@ def product_page(request):
     products = Product.objects.all()
     return render(request, 'product/product-page.html',{
         'products': products
+    })
+
+def get_all_cateogries(request):
+    categories = Category.objects.all()
+    return render(request, 'product/categories.html',{
+        'categories': categories
     })
