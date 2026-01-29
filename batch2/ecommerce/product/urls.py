061f8ebd2bb325131c3dcd230d69430bd9e0558b
Sachin Barali <baralisachin5925@gmail.com>
2026-01-29 13:09:04 +0545

    category delete done

--- batch2/ecommerce/product/urls.py
+++ batch2/ecommerce/product/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('',views.product_page),
-   path('categories/',views.get_all_cateogries),
+   path('',views.product_page,name='product_page'),
+   path('categories/',views.get_all_cateogries,name='all_categories'),
+   path('delete-category/<int:category_id>/',views.delete_category,name='delete_category')
 ]
--- batch2/ecommerce/product/views.py
+++ batch2/ecommerce/product/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Product,Category
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -14,3 +15,11 @@ def get_all_cateogries(request):
     return render(request, 'product/categories.html',{
         'categories': categories
     })
+
+def delete_category(request,category_id):
+    category = Category.objects.get(id=category_id)
+    category.delete()
+    return redirect('all_categories')
+
+# def dynamic_url(request,category_id):
+#     return HttpResponse(f"dynamic url ===> {category_id}")
