c158de60c1f201b23cc0bd123098f557ef3de0d0
Sachin Barali <baralisachin5925@gmail.com>
2026-02-13 13:18:20 +0545

    class based views

--- batch2/ecommerce/product/urls.py
+++ batch2/ecommerce/product/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('',views.product_page,name='product_page'),
-   path('add-product/',views.post_product,name='add_product'),
+   path('',views.ProductPageView.as_view(),name='product_page'),
+   path('add-product/',views.CreateProductView.as_view(),name='add_product'),
    path('edit-product/<int:product_id>/',views.edit_product, name='edit_product'),
-   path('<int:product_id>/',views.delete_product,name='delete_product'),
+   path('<int:pk>/',views.DeleteProductView.as_view(),name='delete_product'),
    path('categories/',views.get_all_cateogries,name='all_categories'),
    path('delete-category/<int:category_id>/',views.delete_category,name='delete_category'),
    path('login-form/',views.form_practise, name='login-form')
--- batch2/ecommerce/product/views.py
+++ batch2/ecommerce/product/views.py
@@ -1,16 +1,54 @@
 from django.shortcuts import render,redirect
+from django.urls import reverse_lazy
 from .models import Product,Category,LoginPractise
 from product.forms import LoginForm,ProductForm,EditProductForm
+from django.views import View
+from django.views.generic import ListView,CreateView,DeleteView,UpdateView
 
-# Create your views here.
 
-def product_page(request):
-    products = Product.objects.all()
-    return render(request, 'product/product-page.html',{
-        'products': products,
-        "product_length": len(products) > 0
-    })
+# class based views
+class ProductPageView(ListView):
+    """class based views for product page."""
+    model = Product
+    template_name = 'product/product-page.html'
 
+    def get_context_data(self, **kwargs):
+        products = Product.objects.all()
+        return {
+            'products': products,
+            'product_length': len(products) > 0
+        }
+    # def get(self,request):
+    #     products = Product.objects.all()
+    #     return render(request, 'product/product-page.html', {
+    #         'products': products,
+    #         'product_length': len(products) > 0
+    #     })
+
+
+# def product_page(request):
+#     products = Product.objects.all()
+#     return render(request, 'product/product-page.html',{
+#         'products': products,
+#         "product_length": len(products) > 0
+#     })
+
+
+class CreateProductView(CreateView):
+    """Class based view to create the product"""
+    # def get(self,request):
+    #     form = ProductForm()
+    #     return render(request, 'product/product-form.html',{'form': form})
+
+    # def post(self,request):
+    #     form = ProductForm(data=request.POST,files=request.FILES)
+    #     if form.is_valid():
+    #         form.save()
+    #     return redirect('product_page')
+    model = Product
+    template_name = 'product/product-form.html'
+    form_class = ProductForm
+    success_url = reverse_lazy('product_page')
 
 def post_product(request):
     """Method to add/create the product."""
@@ -37,6 +75,13 @@ def edit_product(request,product_id):
     return render(request, "product/edit-product.html",{'form': form})
 
 
+class DeleteProductView(DeleteView):
+    """Class based views to delete the product"""
+    model = Product
+    success_url = reverse_lazy('product_page')
+    template_name = 'product/confirm_delete.html'
+
+
 def delete_product(request,product_id):
     product = Product.objects.get(id=product_id)
     product.delete()
@@ -51,7 +96,8 @@ def get_all_cateogries(request):
 
 # post category
 
-# edit category 
+# edit category
+
 
 def delete_category(request,category_id):
     category = Category.objects.get(id=category_id)
